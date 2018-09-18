6b05976a4416fb06704b33bc944237bb813d9e51
Felix Manuel <luxotus@gmail.com>
2018-09-17 21:41:41 -0400

    setup function for getting all permutations. Working on tests to verify it

--- src/general-stat-helper.js
+++ src/general-stat-helper.js
@@ -1,4 +1,8 @@
 const generalStatHelper = {
+  /**
+   * the product of an integer and all the integers below it
+   * @param {int} num
+   */
   factorial: (num) => {
     if (num < 0) {
       return -1;
@@ -10,8 +14,27 @@ const generalStatHelper = {
 
     return (num * generalStatHelper.factorial(num - 1));
   },
-  sampleArrangement: () => {},
-  permutations: () => {},
+
+  /**
+   * Provides all permutations of a set
+   * @param {array} arr
+   */
+  permutations: (arr) => {
+    const ret = [];
+
+    for (let i = 0; i < arr.length; i += 1) {
+      const rest = generalStatHelper.permutations(arr.slice(0, i).concat(arr.slice(i + 1)));
+
+      if (!rest.length) {
+        ret.push([arr[i]]);
+      } else {
+        for (let j = 0; j < rest.length; j += 1) {
+          ret.push([arr[i]].concat(rest[j]));
+        }
+      }
+    }
+    return ret;
+  },
   combination: () => {},
   binomialCoefficient: () => {},
 };
--- test/general-stat-helper.test.js
+++ test/general-stat-helper.test.js
@@ -1,10 +1,20 @@
 import assert from 'assert';
 import generalStatHelper from '../src/general-stat-helper';
 
+// console.log(generalStatHelper.permutations(['a', 'b', 'c']));
+
 describe('generalStatHelper', function() {
   describe('factorial', function() {
     it('4! = 24', function() {
       assert.equal(generalStatHelper.factorial(4), 24);
+    });
+  });
+  describe('permutations', function() {
+    const arr = ['a', 'b', 'c'];
+    const factorial = generalStatHelper.factorial(arr.length);
+
+    it(`[${arr.join(', ')}]! = ${factorial}`, function() {
+      assert.equal(generalStatHelper.permutations(arr).length, factorial);
     });
   });
 });
