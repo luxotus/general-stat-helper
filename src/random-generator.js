cebe763279c866596766eb48c6401e513f09c46c
Felix Manuel <luxotus@gmail.com>
2018-09-21 22:53:10 -0400

    Fixed issue with random gen but will need to expand tests

--- src/random-generator.js
+++ src/random-generator.js
@@ -1,16 +1,31 @@
 /**
  * Generates a set of random values that matches input values probability
  * Ex. flip a coin 50 times and get exactly 50% heads and 50% tails
+ * @param {array} seq sequence of possible values
+ * @param {int} discreteEvents number of separate events
  */
 export default class randomGenerator {
-  constructor(value, size) {
+  constructor(seq, discreteEvents) {
     this.currentIndex = 0;
-    this.value = value;
-    this.size = size;
+    this.seq = seq;
+    this.discreteEvents = discreteEvents;
+    const size = Math.floor(discreteEvents / seq.length);
+    // let isOdd = (discreteEvents / seq.length) % 2 !== 0;s
+    let differenceToMax = Math.abs(discreteEvents - size * seq.length);
+    let increment = 0;
 
-    // Creates a sequence starting from 1
-    this.startSequence = Array.from(new Array(value), (val, index) => index + 1);
-    this.possibleSets = this.startSequence.map(val => Array(size / value).fill(val));
+    console.log(`differenceToMax: ${differenceToMax}`);
+
+    this.possibleSets = this.seq.map((val) => {
+      if (differenceToMax > 0) {
+        differenceToMax -= 1;
+        increment = 1;
+      } else {
+        increment = 0;
+      }
+
+      return Array(size + increment).fill(val);
+    });
     this.unShuffledSet = [].concat(...this.possibleSets);
     this.shuffledSet = this.unShuffledSet.map(a => [Math.random(), a])
       .sort((a, b) => a[0] - b[0]).map(a => a[1]);
@@ -18,7 +33,7 @@ export default class randomGenerator {
 
   get rand() {
     const currentVal = this.shuffledSet[this.currentIndex];
-    this.currentIndex = (this.currentIndex >= this.size) ? 0 : this.currentIndex + 1;
+    this.currentIndex = (this.currentIndex >= this.discreteEvents) ? 0 : this.currentIndex + 1;
 
     return currentVal;
   }
--- test/random-generator.test.js
+++ test/random-generator.test.js
@@ -2,13 +2,25 @@ import assert from 'assert';
 import randomGenerator from '../src/random-generator';
 
 describe('Random Generator', function() {
-  const numOfRolls = 60;
-  const sides = 6;
-  const randomGen = new randomGenerator(sides, numOfRolls);
+  const numOfRolls = [100, 60, 37];
+  const sides = [1, 2, 3, 4, 5, 6];
+  const randomGen = [
+    new randomGenerator(sides, numOfRolls[0]),
+    new randomGenerator(sides, numOfRolls[1]),
+    new randomGenerator(sides, numOfRolls[2]),
+  ];
+
+  // console.log(randomGen);
 
   describe('Shuffled Array has the same specified size', function() {
-    it('Rolling dice 60 times', function() {
-      assert.equal(randomGen.shuffledSet.length, numOfRolls);
+    it(`Rolling dice ${numOfRolls[0]} times`, function() {
+      assert.equal(randomGen[0].shuffledSet.length, numOfRolls[0]);
+    });
+    it(`Rolling dice ${numOfRolls[1]} times`, function() {
+      assert.equal(randomGen[1].shuffledSet.length, numOfRolls[1]);
+    });
+    it(`Rolling dice ${numOfRolls[2]} times`, function() {
+      assert.equal(randomGen[2].shuffledSet.length, numOfRolls[2]);
     });
   });
 });
